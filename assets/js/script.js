f7c793de83382d5d4fa60c07981fdf9a18397d44
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-08-20 09:56:58 -0600

    Appending current weather- in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,15 @@ var cityInputEl = document.getElementById('cityInput');
 var searchEl = document.getElementById('searchButton');
 var apiKey = "3a41fb29e2c055f4d8aacef579f499c3";
 
+var currentHeaderEl = document.getElementById('cityDateCurrent');
+var currentTempEl = document.getElementById('currentTemp');
+var currentWindEl = document.getElementById('currentWind');
+var currentHumidityEl = document.getElementById('currentHumidity');
+var currentUVEl = document.getElementById('currentUV');
 //Capture user input and fetch weather for that city
 searchEl.addEventListener('click', function() {
     var city = cityInputEl.value;
-    // console.log(city);
-    var fetchWeather = fetch(
+    fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
     )
     .then(function (response) {
@@ -17,11 +21,20 @@ searchEl.addEventListener('click', function() {
     })
     .then(function (data) {
         console.log(data);
+        if (data.cod !== 200){
+            console.log("sorry");
+        } else {
+            currentHeaderEl.textContent = city + " " + moment().format("MMM Do YY") + " " + data.weather[0].icon;
+            console.log(data.weather[0].icon);
+        }
     })
+    return;
 });
 
 
-
+// function showWeather() {
+//     console.log(data);
+// }
 
 
 
